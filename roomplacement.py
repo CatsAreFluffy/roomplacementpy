52ca58d9667a491dda985d150c24bdbc245c96b7
CatsAreFluffy <catisfluffy@gmail.com>
2017-01-16 17:57:39 -0800

    0.0.3 is here!
    Added window and table, left/right to switch

--- roomplacement.py
+++ roomplacement.py
@@ -1,4 +1,4 @@
-#Room Placement Python v0.0.2
+#Room Placement Python v0.0.3
 #Not equivalent to any Room Placement Scratch version
 #https://scratch.mit.edu/projects/137918573/
 import tkinter as tk
@@ -7,17 +7,34 @@ class Application(tk.Frame):
         super().__init__(master)
         self.scale=2
         self.objects=[]
+        self.selected=0
         self.pack()
         self.create_widgets()
     def create_widgets(self):
         self.draw=tk.Canvas(self,width=480*self.scale,height=360*self.scale,background="#eeeeee")
         self.draw.pack()
         def place(event):
-            self.objects+=[[self.draw.create_rectangle(event.x-32*self.scale,event.y-42*self.scale,event.x+32*self.scale,event.y+42*self.scale,fill="#aa6600",outline="#aa6600"),\
-                           self.draw.create_rectangle(event.x-30*self.scale,event.y-20*self.scale,event.x+30*self.scale,event.y+40*self.scale,fill="#ff0000",outline="#ff0000"),\
-                           self.draw.create_rectangle(event.x-30*self.scale,event.y-40*self.scale,event.x+30*self.scale,event.y-20*self.scale,fill="#ffffff",outline="#ffffff")]]
+            if self.selected==0:
+                self.objects+=[[self.draw.create_rectangle(event.x-32*self.scale,event.y-42*self.scale,event.x+32*self.scale,event.y+42*self.scale,fill="#cc8800",outline="#cc8800"),\
+                                self.draw.create_rectangle(event.x-30*self.scale,event.y-20*self.scale,event.x+30*self.scale,event.y+40*self.scale,fill="#ff0000",outline="#ff0000"),\
+                                self.draw.create_rectangle(event.x-30*self.scale,event.y-40*self.scale,event.x+30*self.scale,event.y-20*self.scale,fill="#ffffff",outline="#ffffff")]]
+            elif self.selected==1:
+                self.objects+=[[self.draw.create_rectangle(event.x-20*self.scale,event.y-20*self.scale,event.x+20*self.scale,event.y+20*self.scale,fill="#cc8800",outline="#cc8800"),\
+                                self.draw.create_rectangle(event.x-10*self.scale,event.y- 5*self.scale,event.x+10*self.scale,event.y+20*self.scale,fill="#eeeeee",outline="#eeeeee")]]
+            elif self.selected==2:
+                self.objects+=[[self.draw.create_rectangle(event.x-40*self.scale,event.y-30*self.scale,event.x+40*self.scale,event.y+30*self.scale,fill="#e6e6ff",outline="#aaaaaa",width=2)]]
             print(self.objects)
         self.draw.bind("<Button-1>",place)
+        def nextobject(event):
+            self.selected+=1
+            self.selected%=3
+            print(self.selected)
+        def prevobject(event):
+            self.selected-=1
+            self.selected%=3
+            print(self.selected)
+        self.master.bind("<Right>",nextobject)
+        self.master.bind("<Left>", prevobject)
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
